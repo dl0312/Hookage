ee6c4fb82e2f91f0f4c3f3c48899465273e60a4f
GeonLee <leegun2003@gmail.com>
2019-06-06 22:10:47 +0900

    update: useTabs

---
+++ example/UseInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import useInput from "../useInput";
+
+const UseInput = () => {
+  const maxLen = value => value.length <= 10;
+  const name = useInput("", maxLen);
+  return (
+    <div className="App">
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+export default UseInput;
