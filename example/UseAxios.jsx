879b6179013012f84da4b73f619a055023dcb5d4
GeonLee <leegun2003@gmail.com>
2019-06-07 16:54:33 +0900

    update: useAxios

---
+++ example/UseAxios.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useAxios } from "../useAxios";
+
+const UseAxios = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.am/api/v2/list_movies.json"
+  });
+  return (
+    <div className="App">
+      <button onClick={refetch}>refetch</button>
+      <div>{data && JSON.stringify(data.data.data.movies)}</div>
+      <h2>{error && "error"}</h2>
+      <h2>{loading && "Loading"}</h2>
+    </div>
+  );
+};
+
+export default UseAxios;
